930c07b080a10cff83ebb94bc6aa2a549af79a79
shohei-kasahara <shohei.kasahara0405@gmail.com>
2026-05-21 02:43:13 +1000

    Find the buttons and text view by their IDs

--- app/src/main/java/io/github/shohei/calculator/MainActivity.kt
+++ app/src/main/java/io/github/shohei/calculator/MainActivity.kt
@@ -1,6 +1,9 @@
 package io.github.shohei.calculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Find the buttons and text view by their IDs
+
+
+
     }
+
 }
